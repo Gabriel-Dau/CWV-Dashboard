bc97adfb6630b3a73b47cf7249729cc2d3f9a5db
Gabriel-Dau <gabrielqdau@yahoo.com>
2024-06-25 14:56:32 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+
+// import fs from 'fs';
+// import DataTable from 'react-data-table-component';
 import './App.css';
 
+// const path = 'C:/Users/A0806223/CWV.txt';
+
+// const read = async () => fs.readFile(path, (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     throw err;
+//   } else {
+//     try {
+//       return JSON.parse(data);
+//     } catch (error) {
+//       console.error(error);
+//       throw error;
+//     }
+//   }
+// });
+
 function App() {
+  // const data = read();
   return (
     <div className="App">
      
