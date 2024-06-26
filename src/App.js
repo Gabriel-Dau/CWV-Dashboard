224856bf95ac577f29b0504a0ca038a8b587d3f4
Gabriel-Dau <gabrielqdau@yahoo.com>
2024-06-26 08:30:28 -0500

    Added table with 5 columns

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import DataTable from 'react-data-table-component';
 import React from 'react';
 import './App.css';
 
@@ -5,16 +6,43 @@ function App() {
   const [data, setData] =  React.useState({});
 
   React.useEffect(() => {
-    fetch('http://localhost:3000').then(response => response.json().then(data => setData(data)));
+    fetch('http://localhost:3000').then(response => response.json().then(data => {
+      setData(Object.keys(data).map(key => data[key]));
+    }));
   }, []);
 
-  console.log(data);
+  const columns = [
+    {
+      name: 'Value',
+      selector: row => row.value, 
+    },
+    {
+      name: 'Rating',
+      selector: row => row.rating,
+    },
+    {
+      name: 'Delta',
+      selector: row => row.delta,
+    },
+    {
+      name: 'URL',
+      selector: row => row.url,
+    },
+    {
+      name: 'Date',
+      selector: row => row.date,
+    }
+  ];
 
   return (
     <div className="App">
-     
+     <DataTable
+      columns={columns}
+      data={data}
+     />
     </div>
   );
 }
 
 export default App;
+// save
