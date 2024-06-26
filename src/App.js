a2be2587b4f6f67888fce751690d516e7f8ed71e
Gabriel-Dau <gabrielqdau@yahoo.com>
2024-06-26 11:40:00 -0500

    Added sorting and pagination. Pagination has bug that duplicates items when changing pages.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import DataTable from 'react-data-table-component';
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
-  const [data, setData] =  React.useState({});
+  const [data, setData] =  React.useState([]);
 
   React.useEffect(() => {
     fetch('http://localhost:3000').then(response => response.json().then(data => {
@@ -14,31 +16,40 @@ function App() {
   const columns = [
     {
       name: 'Value',
+      sortable: true,
       selector: row => row.value, 
     },
     {
       name: 'Rating',
       selector: row => row.rating,
+      sortable: true,
     },
     {
       name: 'Delta',
       selector: row => row.delta,
+      sortable: true,
     },
     {
       name: 'URL',
       selector: row => row.url,
+      sortable: true,
     },
     {
       name: 'Date',
       selector: row => row.date,
+      sortable: true,
     }
   ];
-
+  
+  // const Basic = () => <DataTable title="Interaction to Next Paint" columns={columns} data={data} pagination />;
+  
   return (
     <div className="App">
-     <DataTable
-      columns={columns}
-      data={data}
+      <h2>Interaction to Next Paint</h2>
+      <DataTable
+        columns={columns}
+        data={data}
+        pagination
      />
     </div>
   );
