615e5935995255b88d9fcb203924422b48c1ed86
Joshua Wilson <joshua.wilson@academy.com>
2024-07-22 10:41:43 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,54 @@ import DataTable from 'react-data-table-component';
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
-import { Button } from '@mui/material';
+
+const TextField = styled.input`
+  height: 32px;
+  width: 200px;
+  border-radius: 3px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  border: 1px solid #e5e5e5;
+  padding: 0 32px 0 16px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ClearButton = styled('button')`
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  height: 34px;
+  width: 34px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #2979ff;
+  border: none;
+  color: white;
+`;
+
+const FilterComponent = ({ filterText, onFilter, onClear }) => (
+  <>
+    <TextField
+      id="search"
+      type="text"
+      placeholder="Filter By URL"
+      aria-label="Search Input"
+      value={filterText}
+      onChange={onFilter}
+    />
+    <ClearButton type="button" onClick={onClear}>
+      X
+    </ClearButton>
+  </>
+);
 
 const ExpandedContent = ({ data }) => {
   const items = ['value', 'rating', 'url', 'date', 'delta', 'navigationType', 'id', 'DOMpath', 'userAgent']
@@ -17,7 +64,7 @@ const ExpandedContent = ({ data }) => {
     
   </table>
   )
-}
+};
   
 function App() {
   const [data, setData] =  React.useState([]);
@@ -37,7 +84,6 @@ function App() {
     {
       name: 'Rating',
       selector: row => row.rating,
-      sortable: true,
     },
     {
       name: 'Date',
@@ -45,7 +91,7 @@ function App() {
       sortable: true,
     }
   ];
-    
+
   const conditionalRowStyles = [
     {
       when: row => row.value <= 200,
@@ -69,52 +115,7 @@ function App() {
         color: 'white'
       },
     }
-  ]
-
-  const TextField = styled.input`
-    height: 32px;
-    width: 200px;
-    border-radius: 3px;
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    border: 1px solid #e5e5e5;
-    padding: 0 32px 0 16px;
-
-    &:hover {
-      cursor: pointer;
-    }
-  `;
-
-  const ClearButton = styled(Button)`
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
-    height: 34px;
-    width: 32px;
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
-
-  const FilterComponent = ({ filterText, onFilter, onClear }) => (
-    <>
-      <TextField
-        id="search"
-        type="text"
-        placeholder="Filter By Url"
-        aria-label="Search Input"
-        value={filterText}
-        onChange={onFilter}
-      />
-      <ClearButton type="button" onClick={onClear}>
-        X
-      </ClearButton>
-    </>
-  );
+  ];
   
   const [filterText, setFilterText] = React.useState('');
   const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
@@ -132,22 +133,20 @@ function App() {
 
   return (
     <div className="App">
-      <div className="title">
-        <h1>Interaction to Next Paint</h1>
-      </div>
       <div className='dataTable'>
         <DataTable
-        columns={columns}
-        data={filteredItems}
-        theme="dark"
-        pagination
-        paginationResetDefaultPage={resetPaginationToggle}
-        subHeader
-        subHeaderComponentMemo={subHeaderComponentMemo}
-        persistTableHead={true}
-        conditionalRowStyles={conditionalRowStyles}
-        expandableRows
-        expandableRowsComponent={ExpandedContent}
+          title="Interaction to Next Paint"
+          columns={columns}
+          data={filteredItems}
+          theme="dark"
+          pagination
+          paginationResetDefaultPage={resetPaginationToggle}
+          subHeader
+          subHeaderComponent={subHeaderComponentMemo}
+          persistTableHead={true}
+          conditionalRowStyles={conditionalRowStyles}
+          expandableRows
+          expandableRowsComponent={ExpandedContent}
         />
      </div>
     </div>
